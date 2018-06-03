e4bb7202f5c682ddab08581a0ffcb9295e1f618b
millervm <vmcmiller@gmail.com>
2018-06-03 21:19:36 +0000

    added associations to models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+    belongs_to :list
+    belongs_to :user
+    
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,5 @@
 class List < ApplicationRecord
+    belongs_to :user
+    has_many :items
+    
 end

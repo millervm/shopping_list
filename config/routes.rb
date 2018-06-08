0f895724cf2679d1d5a5cc404f1bd2d4f7b50166
millervm <vmcmiller@gmail.com>
2018-06-08 05:22:53 +0000

    updated routes and added views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :users, only: [:new, :show] do
+  resources :users, only: [:new, :show, :edit] do
     resources :lists, only: [:new, :create, :index]
   end
   
   resources :lists, only: [:show, :edit, :update, :destroy] do
-    resources :items, only: [:new, :create, :index]
+    resources :items, only: [:new, :create]
   end
   
   resources :items, only: [:show, :edit, :update, :destroy]
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/sessions', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  
+  root 'application#home'
   
 end

af971672260c710cc34addb31769acd66bf589bb
millervm <vmcmiller@gmail.com>
2018-06-16 20:07:46 +0000

    added missing end bracket to password length validation line

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
     validates :name, presence: true, message: "You must enter a name."
     validates :name, {:length => {maximum: 50}, message: "Your name must have less than 50 characters."}
     validates :password, presence: true, message: "You must enter a password."
-    validates :password, {:length => {in: 6..50}, message: "Your password must have 6-40 characters."
+    validates :password, {:length => {in: 6..50}, message: "Your password must have 6-40 characters."}
     
 end

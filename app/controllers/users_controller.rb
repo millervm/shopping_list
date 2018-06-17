06fad5392052db1c74170d26643d5ca3409c5efb
millervm <vmcmiller@gmail.com>
2018-06-17 22:19:57 +0000

    added linebreaks to flash notices for multiple error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,31 @@
 class UsersController < ApplicationController
     
     def new
-        @user = User.new
+        if logged_in?
+            flash[:notice] = "You're already logged in!"
+            redirect_to user_path(current_user)
+        else
+            @user = User.new
+        end
     end
     
     def create
-        if params[:user].values.include?("")
-            flash[:notice] =  "All fields are required."
-            redirect_to new_user_path
-        else
+        if !logged_in?
             @user = User.new(user_params)
             if @user.save
                 session[:user_id] = @user.id
                 redirect_to user_path(@user)
             else
-                flash[:notice] = "Could not create account. Please try again."
+                #@user.errors.full_messages.each do |message|
+                    #flash[:notice] =[]
+                    #flash[:notice] << message
+                #end
+                flash[:notice] = @user.errors.messages.values.flatten
+                #flash[:notice] = @user.errors.messages.values.flatten.collect {|message| message + "<br>"}
                 redirect_to new_user_path
             end
+        else
+            redirect_to user_path(current_user)
         end
     end
     

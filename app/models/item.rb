d4e37ec30cbffabeb4408865150b766f9d57fc82
millervm <vmcmiller@gmail.com>
2018-06-19 05:33:20 +0000

    corrected validation details

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
     belongs_to :list
     belongs_to :user
     
-    validates :name, presence: true, message: "Your item must have a name."
-    validates :description, {:length => {maximum: 500}, message: "Your description must have less than 500 characters."}
+    validates :name, presence: {message: "Your item must have a name."}
+    validates :description, length: {maximum: 500, message: "Your description must have less than 500 characters."}
 end

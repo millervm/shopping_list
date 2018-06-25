550341035f104bd2a770df8e288ccfc5ea059b9d
millervm <vmcmiller@gmail.com>
2018-06-25 06:24:38 +0000

    added urgent_items method and option to list urgent items on lists index page

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,25 @@ class List < ApplicationRecord
     belongs_to :user
     has_many :items
     
-    validates :name, presence: true, message: "List must have a name."
-    validates :name, uniqueness: true, scope: user, message: "You cannot have two lists with the same name."
+    validates :name, presence: {message: "List must have a name."}
+    validates :user_id, uniqueness: {scope: :name, message: "You cannot have two lists with the same name."}
+    
+    def complete_items
+        self.items.collect do |item|
+            iten if item.complete
+        end
+    end
+    
+    def incomplete_items
+        self.items.collect do |item|
+            item if !item.complete
+        end
+    end
+    
+    def urgent_items
+        self.incomplete_items.collect do |item|
+            item if item.urgent
+        end
+    end
+    
 end

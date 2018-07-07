d8ee429784fc73d33e73f2aeb824e6e1d3ed23b7
millervm <vmcmiller@gmail.com>
2018-07-07 21:48:08 +0000

    added #urgent_list_items for user instances and display to user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ApplicationRecord
-    has_secure_password
+    has_secure_password(validations: false)
     has_many :lists
     has_many :items, through: :lists
     
-    validates :name, presence: true, message: "You must enter a name."
-    validates :name, {:length => {maximum: 50}, message: "Your name must have less than 50 characters."}
-    validates :password, presence: true, message: "You must enter a password."
-    validates :password, {:length => {in: 6..50}, message: "Your password must have 6-40 characters."}
+    validates :name, presence: {message: "You must enter a name."}
+    validates :name, length: {maximum: 50, message: "Your name must have less than 50 characters."}
+    validates :password, presence: {message: "You must enter a password."}
+    validates :password, length: {in: 6..50, message: "Your password must have 6-40 characters."}
+    
+    def urgent_list_items
+        urgent = {}
+        self.lists.each do |list|
+            urgent[list] = list.urgent_items.collect {|item| item} if !list.urgent_items.empty?
+        end
+        urgent
+    end
     
 end

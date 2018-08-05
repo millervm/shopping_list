6ee795fcb8431d4b892637c25e6c39c88b6ef92e
millervm <vmcmiller@gmail.com>
2018-08-05 21:59:27 +0000

    removed default validator for belongs_to user and added presence validator with custom error message

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,10 @@
 class List < ApplicationRecord
-    belongs_to :user
+    belongs_to :user, optional: true
     has_many :items
     
     validates :name, presence: {message: "List must have a name."}
     validates :user_id, uniqueness: {scope: :name, message: "You cannot have two lists with the same name."}
+    validates :user, presence: {message: "List must belong to a current user."}
     
     def complete_items
         self.items.select do |item|

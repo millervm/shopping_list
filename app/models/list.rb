26cd019f541bf5b2469f2e4e93035ae8a62b6950
millervm <vmcmiller@gmail.com>
2018-09-15 20:34:37 +0000

    updated user_id validation and error message

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,9 +2,9 @@ class List < ApplicationRecord
     belongs_to :user, optional: true
     has_many :items
     
-    validates :name, presence: {message: "List must have a name."}
-    validates :user_id, uniqueness: {scope: :name, message: "You cannot have two lists with the same name."}
-    validates :user, presence: {message: "List must belong to a current user."}
+    validates :name, presence: {message: "List must have a name."},
+                     uniqueness: {scope: :user_id, case_sensitive: false, message: "You cannot have two lists with the same name."}
+    validates :user_id, presence: {message: "User must be specified."}
     
     def complete_items
         self.items.select do |item|

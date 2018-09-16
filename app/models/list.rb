9e5d27c995a01ecf5d20172f5be219d50566b946
millervm <vmcmiller@gmail.com>
2018-09-16 21:56:02 +0000

    added active and inactive methods to List class

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,10 +2,16 @@ class List < ApplicationRecord
     belongs_to :user, optional: true
     has_many :items
     
-    #add active/current attribute, to segregate those "on hold"??
-    
     validates :name, presence: {message: "List must have a name."},
                      uniqueness: {scope: :user_id, case_sensitive: false, message: "You cannot have two lists with the same name."}
     validates :user_id, presence: {message: "User must be specified."}
+    
+    def self.active
+        where(active: true)
+    end
+    
+    def self.inactive
+        where(active: false)
+    end
     
 end

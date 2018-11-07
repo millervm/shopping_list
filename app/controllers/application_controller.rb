506d77f489989679fbc441892b388b04911e3a2f
millervm <vmcmiller@gmail.com>
2018-11-07 17:41:52 +0000

    update current_user and logged_in? methods for more efficient checks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,11 @@ class ApplicationController < ActionController::Base
     private
     
         def current_user
-            User.find_by(id: session[:user_id])    
+            @current_user ||= User.find_by(id: session[:user_id])   
         end
         
         def logged_in?
-            session[:user_id]
+            !!current_user
         end
     
         def require_login
